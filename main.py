636b2aeb51cb27aed964aec1639be5722b2bd927
Muhammad Zain <zainuet2022@gmail.com>
2026-09-14 22:14:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, HTTPException, Query, status
 from pydantic import BaseModel, Field
 
 app = FastAPI(
-    title="To-Do List API",
+    title="To-Do List APP API",
     description="A lightweight, containerized To-Do List API built for DevOps & Cloud demonstration.",
     version="1.0.0",
 )
